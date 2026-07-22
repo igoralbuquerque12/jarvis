9f13f5e8d72864ca6cab396ff848c80f530e183b
Igor Albuquerque <igorcostalins2005@gmail.com>
2026-07-22 01:45:07 -0300

    feat(app): register Schedule and Events modules

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,23 +1,27 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AuthModule } from './auth/auth.module';
 import { MessagesModule } from './messages/messages.module';
 import { ProfileModule } from './profile/profile.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { SubscriptionModule } from './subscription/subscription.module';
 import { WhatsappModule } from './whatsapp/whatsapp.module';
+import { EventsModule } from './events/events.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    ScheduleModule.forRoot(),
     PrismaModule,
     AuthModule,
     ProfileModule,
     SubscriptionModule,
     MessagesModule,
     WhatsappModule,
+    EventsModule,
   ],
   controllers: [],
   providers: [],
