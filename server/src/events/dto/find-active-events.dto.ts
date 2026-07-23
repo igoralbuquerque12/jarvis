ebd43770b70d2b46bad259591bcc8dda73369eb7
Igor Albuquerque <igorcostalins2005@gmail.com>
2026-07-23 02:08:20 -0300

    refactor(events): update FindActiveEventsDto to use profileId and remove timezone

--- server/src/events/dto/find-active-events.dto.ts
+++ server/src/events/dto/find-active-events.dto.ts
@@ -1,14 +1,13 @@
 import { EventSeriesType } from '@prisma/client';
-import { IsEnum, IsOptional, IsString, Matches } from 'class-validator';
+import { IsEnum, IsOptional, IsUUID, Matches } from 'class-validator';
 
 export class FindActiveEventsDto {
+  @IsUUID()
+  profileId: string;
+
   @IsOptional()
   @Matches(/^\d{4}-\d{2}-\d{2}$/)
   scheduledAt?: string;
-
-  @IsOptional()
-  @IsString()
-  timezone?: string;
 
   @IsOptional()
   @IsEnum(EventSeriesType)
