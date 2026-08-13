d3cac9a2eac5e51b0b5f3a30750b9258fc4e98b6
Igor Albuquerque <igorcostalins2005@gmail.com>
2026-08-13 20:31:05 -0300

    chore(web): delete old home page

--- web/src/pages/home.tsx
+++
@@ -1,9 +0,0 @@
-export function HomeRoute() {
-  return (
-    <main className="app-shell app-shell--centered">
-      <section className="home-panel">
-        <h1>Hello, world</h1>
-      </section>
-    </main>
-  );
-}
