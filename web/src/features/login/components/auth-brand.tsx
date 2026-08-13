7e84b19022bb5ec0021371e0be2bb63f9d9aa36f
Igor Albuquerque <igorcostalins2005@gmail.com>
2026-08-13 20:30:59 -0300

    chore(auth): delete old login feature components and hooks

--- web/src/features/login/components/auth-brand.tsx
+++
@@ -1,9 +0,0 @@
-import logoImage from '../../../../../ui ux/logo image jarvis.png';
-
-export function AuthBrand() {
-  return (
-    <div className="auth-brand">
-      <img className="auth-brand__image" src={logoImage} alt="Jarvis, seu assistente pessoal" />
-    </div>
-  );
-}
