1af3894ca0cb93bc1cd6ff625718afb5b8aa7d43
Igor Albuquerque <igorcostalins2005@gmail.com>
2026-08-13 20:30:58 -0300

    refactor(auth): remove old login and reset password routes

--- web/src/pages/login.tsx
+++
@@ -1,8 +0,0 @@
-import { LoginPage } from '../features/login/components/login-page';
-import { useLoginPage } from '../features/login/hooks/use-login-page';
-
-export function LoginRoute() {
-  const model = useLoginPage();
-
-  return <LoginPage model={model} />;
-}
--- web/src/pages/reset-password.tsx
+++
@@ -1,8 +0,0 @@
-import { ResetPasswordPage } from '../features/login/components/reset-password-page';
-import { useResetPasswordPage } from '../features/login/hooks/use-reset-password-page';
-
-export function ResetPasswordRoute() {
-  const model = useResetPasswordPage();
-
-  return <ResetPasswordPage model={model} />;
-}
