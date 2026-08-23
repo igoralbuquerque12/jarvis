8b9590aaa4e6ecc757bf0628f05ab741ee0a3ca9
Igor Albuquerque <igorcostalins2005@gmail.com>
2026-08-23 01:56:38 -0300

    refactor(profile): update profile module imports

--- server/src/profile/profile.module.ts
+++ server/src/profile/profile.module.ts
@@ -1,11 +1,11 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
-import { ProfileController } from './profile.controller';
-import { ProfileService } from './profile.service';
+import { ProfileController } from './controllers/profile.controller';
+import { ProfileService } from './services/profile.service';
 import { SubscriptionModule } from '../subscription/subscription.module';
 
 @Module({
-  imports: [forwardRef(() => AuthModule), SubscriptionModule],
+  imports: [forwardRef(() => AuthModule), forwardRef(() => SubscriptionModule)],
   controllers: [ProfileController],
   providers: [ProfileService],
   exports: [ProfileService],
