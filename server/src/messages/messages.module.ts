227ad19bf2fae9b332c3773190c38121d71b2015
Igor Albuquerque <igorcostalins2005@gmail.com>
2026-08-23 01:56:55 -0300

    refactor: reorganize project structure by moving service and controller files to dedicated directories

--- server/src/messages/messages.module.ts
+++ server/src/messages/messages.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { MessagesService } from './messages.service';
+import { MessagesService } from './services/messages.service';
 
 @Module({
   providers: [MessagesService],
--- server/src/whatsapp/whatsapp.module.ts
+++ server/src/whatsapp/whatsapp.module.ts
@@ -1,11 +1,11 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { BaileysAuthStore } from './auth/baileys-auth.store';
-import { WhatsappAuthCryptoService } from './crypto/whatsapp-auth-crypto.service';
-import { WhatsappController } from './whatsapp.controller';
-import { WhatsappConnectionService } from './whatsapp-connection.service';
-import { WhatsappReceiverService } from './whatsapp-receiver.service';
-import { WhatsappSenderService } from './whatsapp-sender.service';
-import { WhatsappService } from './whatsapp.service';
+import { WhatsappAuthCryptoService } from './services/whatsapp-auth-crypto.service';
+import { WhatsappController } from './controllers/whatsapp.controller';
+import { WhatsappConnectionService } from './services/whatsapp-connection.service';
+import { WhatsappReceiverService } from './services/whatsapp-receiver.service';
+import { WhatsappSenderService } from './services/whatsapp-sender.service';
+import { WhatsappService } from './services/whatsapp.service';
 import { AssistantModule } from '../assistant/assistant.module';
 
 @Module({
