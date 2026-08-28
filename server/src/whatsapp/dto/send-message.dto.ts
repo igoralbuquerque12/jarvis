1ff848ea1dd41a2cc0b4897288b8be6f50c409dc
Igor Albuquerque <igorcostalins2005@gmail.com>
2026-08-28 01:30:10 -0300

    refactor(send-message): remove phone validation and replace with jid property

--- server/src/whatsapp/dto/send-message.dto.ts
+++ server/src/whatsapp/dto/send-message.dto.ts
@@ -3,11 +3,7 @@ import { IsNotEmpty, IsString, Matches } from 'class-validator';
 export class SendMessageDto {
   @IsString()
   @IsNotEmpty()
-  @Matches(/^\d{10,15}$/, {
-    message:
-      'phone must contain only numbers, include country code, and have 10 to 15 digits.',
-  })
-  phone: string;
+  jid: string;
 
   @IsString()
   @IsNotEmpty()
