7d18062420f6f9e0d8c68f5c1e89ce2fe0c56604
Igor Albuquerque <igorcostalins2005@gmail.com>
2026-09-22 23:59:56 -0300

    feat(public-api): add RedisModule to imports in PublicApiModule

--- server/src/public-api/public-api.module.ts
+++ server/src/public-api/public-api.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ApiKeysModule } from '../api-keys/api-keys.module';
+import { RedisModule } from '../redis/redis.module';
 import { WhatsappModule } from '../whatsapp/whatsapp.module';
 import { PublicApiController } from './controllers/public-api.controller';
 import { PublicApiService } from './services/public-api.service';
 
 @Module({
-  imports: [ApiKeysModule, WhatsappModule],
+  imports: [ApiKeysModule, RedisModule, WhatsappModule],
   controllers: [PublicApiController],
   providers: [PublicApiService],
 })
