71a8e54ce958d105af3934e86bd77de29711caee
Igor Albuquerque <igorcostalins2005@gmail.com>
2026-09-23 23:31:54 -0300

    refactor(redis): register RateLimitService in RedisModule

--- server/src/redis/redis.module.ts
+++ server/src/redis/redis.module.ts
@@ -1,9 +1,10 @@
 import { Global, Module } from '@nestjs/common';
+import { RateLimitService } from './services/rate-limit.service';
 import { RedisService } from './services/redis.service';
 
 @Global()
 @Module({
-  providers: [RedisService],
-  exports: [RedisService],
+  providers: [RedisService, RateLimitService],
+  exports: [RedisService, RateLimitService],
 })
 export class RedisModule {}
